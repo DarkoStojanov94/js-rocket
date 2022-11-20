5e83df8a863464ee0cd404f9253129cacb95bc47
DarkoStojanov94 <stojanovdarko94@gmail.com>
2022-11-19 18:48:52 -0600

    Made some car stuff happen!

---
+++ explanations.js
@@ -0,0 +1,52 @@
+// Explanations
+
+//Variables
+
+var fullName = function(firstname, lastname) {
+	return firstname + ' ' + lastname
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var favColor = 'blue';
+var myColors = ['blue', 'red', 'green'];
+var numOfColors = 3;
+var hasGotColors = true;
+var darkObject = {
+	firstName: 'Darko',
+	lastName: 'Stojanov',
+	favColors: ['blue', 'red', 'green'],
+	yearAlive: 103,
+	isMale: true,
+};
+
+// Functions
+
+var whatIsMyFavColor = function () {
+	return false;
+
+	return true;
+
+	return 'blue';
+}
+
+var doSomething = function () {
+	console.log("Do Something!!!")
+}
+
+// Conditions
+
+var name1 = 'Darko';
+var name2 = 'Stojanov';
+
+if (name1 == 'Darko') {
+	alert('true 1!!!');
+} 
+else if (name2 == 'Darko') {
+	alert('true 2!!!');
+}
+else {
+	alert("false");
+};
